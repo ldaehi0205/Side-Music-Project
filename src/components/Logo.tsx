21ab20e6c90d07c26018f11262b46b80360cc227
이대희 <ldaehi0205@gmail.com>
2021-12-22 23:37:22 +0900

    logo add

---
+++ src/components/Logo.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Logo = () => {
+  return <Wrapper>Music</Wrapper>;
+};
+
+export default Logo;
+const Wrapper = styled.div`
+  font-size: 47px;
+  font-family: cursive;
+  font-weight: 700;
+  color: green;
+`;
