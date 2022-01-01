95e0e2cda2041181e27d5e997337d44424fbaf4c
이대희 <ldaehi0205@gmail.com>
2022-01-01 18:43:29 +0900

    nav 추가

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,12 +1,27 @@
 import React from 'react';
+import styled from 'styled-components';
 import Headers from './Header';
+import Nav from './Nav';
+import RecentMusic from './RecentMusic';
 
 const Layout = () => {
   return (
-    <>
+    <Wrapper>
       <Headers />
-    </>
+      <Main>
+        <Nav />
+        <RecentMusic />
+      </Main>
+    </Wrapper>
   );
 };
 
 export default Layout;
+
+const Wrapper = styled.div`
+  position: relative;
+  min-height: 100%;
+`;
+const Main = styled.div`
+  display: flex;
+`;
